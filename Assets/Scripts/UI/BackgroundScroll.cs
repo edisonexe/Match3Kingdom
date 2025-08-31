9b4fbf24695698bdee6d4a47467cd8161debbf5b
ray_rid <rid843@mail.ru>
2025-08-31 18:54:33 +0700

    Add media resources, created ui menu and animate scroll bgimage

---
+++ Assets/Scripts/UI/BackgroundScroll.cs
@@ -0,0 +1,35 @@
+﻿using Cysharp.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI
+{
+    [RequireComponent(typeof(RawImage))]
+    public class BackgroundScroll : MonoBehaviour
+    {
+        [SerializeField] private float _scrollSpeed = 0.007f;
+        [SerializeField] private float _xDirection = 1f;
+        [SerializeField] private float _yDirection = 0f;
+        private RawImage _bgImage;
+        private Vector2 _uvRectSize;
+
+        private async void Awake()
+        {
+            _bgImage = GetComponent<RawImage>();
+            _uvRectSize = _bgImage.uvRect.size;
+            await Scroll().SuppressCancellationThrow();
+        }
+
+        private async UniTask Scroll()
+        {
+            while (destroyCancellationToken.IsCancellationRequested == false)
+            {
+                _bgImage.uvRect = new Rect(
+                    _bgImage.uvRect.position +
+                    new Vector2(_xDirection * _scrollSpeed, _yDirection * _scrollSpeed) *
+                    Time.deltaTime, _uvRectSize);
+                await UniTask.Yield(PlayerLoopTiming.Update, destroyCancellationToken);
+            }
+        }
+    }
+}
