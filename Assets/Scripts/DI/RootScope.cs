65319e6faedb9dbb2e7ed7306b00ad774998255a
ray_rid <rid843@mail.ru>
2025-08-31 17:50:09 +0700

    Added Boot EntryPoint and loading menu scene

---
+++ Assets/Scripts/DI/RootScope.cs
@@ -0,0 +1,22 @@
+﻿using Animations;
+using Boot;
+using SceneLoader;
+using UnityEngine;
+using VContainer;
+using VContainer.Unity;
+
+namespace DI
+{
+    public class RootScope : LifetimeScope
+    {
+        [SerializeField] private LoadingView  _loadingView;
+
+        protected override void Configure(IContainerBuilder builder)
+        {
+            builder.RegisterEntryPoint<BootEntryPoint>();
+            builder.Register<IAsyncSceneLoading, AsyncSceneLoading>(Lifetime.Singleton);
+            builder.Register<IAnimation, AnimationManager>(Lifetime.Singleton);
+            builder.RegisterInstance(_loadingView);
+        }
+    }
+}
